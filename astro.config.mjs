5f8a7c2b9d8fa044acd69c78ea49953d8a613061
xilaluna <xilaluna2@gmail.com>
2025-11-18 19:57:55 -0800

    removed old tailwind for new tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
